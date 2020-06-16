5722477c65776a365d58e259cd05d10a694e6ead
Adrian Fish <adrian.r.fish@gmail.com>
2020-06-16 14:40:30 +0100

    Removed out of date inherits entry

--- src/babel-plugin-append-cachebuster.js
+++ src/babel-plugin-append-cachebuster.js
@@ -3,13 +3,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 
 const whatwgUrl = require("whatwg-url");
-const exportExtensions = require('@babel/plugin-syntax-export-extensions');
 
 /**
  * Appends a cache busting string to any path specifiers.
  */
 exports.appendCacheBuster = (filePath, query) => ({
-  inherits: exportExtensions,
   visitor: {
     'ImportDeclaration|ExportNamedDeclaration|ExportAllDeclaration'(path) {
       const node = path.node;
